407c374a6e6ae05827e1b27621a95dbb9dedc69a
Durai R <duraitech5448@.com>
2024-02-10 13:03:13 +0530

    script-2

--- LearnSkyHostinger/scritp.js
+++
@@ -1,6 +0,0 @@
-let mobile_header = document.querySelector(".header");
-let hamburger = document.querySelector(".mobile_nav");
-
-hamburger.addEventListener("click",function(){
-    mobile_header.classList.toggle("active");
-})
